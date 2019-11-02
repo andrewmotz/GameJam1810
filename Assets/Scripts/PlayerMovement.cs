7ade85f5a6329bf61824495a94228d9750cf1487
Andrew Motz <40045649+andrewmotz@users.noreply.github.com>
2019-11-01 17:24:10 -0700

    Added Controllers and custom mapping
    Added Controllers and custom mapping

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController2D controller;
+    public KeyCode RightAxis = KeyCode.D;
+    public KeyCode LeftAxis = KeyCode.A;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+    bool jump = false;
+    bool crouch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+       
+
+        if (Input.GetKey(RightAxis))
+        {
+            horizontalMove = 1 * runSpeed;
+        } else if (Input.GetKey(LeftAxis))
+        {
+            horizontalMove = -1 * runSpeed;
+        } else
+        {
+            horizontalMove = 0;
+        }
+        Debug.Log(Input.GetAxisRaw("Horizontal"));
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
+        
+    }
+
+    void FixedUpdate()
+    {
+        // Move our character
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+    }
+}
