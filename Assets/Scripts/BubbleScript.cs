a54aa9214185882bb8da0b163d25c467fd2c3f18
Andrew Motz <40045649+andrewmotz@users.noreply.github.com>
2019-11-18 16:52:18 -0800

    Added EvilBubble

---
+++ Assets/Scripts/BubbleScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleScript : MonoBehaviour
+{
+
+    public Animator animator;
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other){
+        Debug.Log("hit");
+        animator.SetTrigger("hit");
+    }
+}
