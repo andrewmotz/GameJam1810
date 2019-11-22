b1b336331c813b3a407d7c389db9af044b44efa9
Andrew Motz <andrewmozza555@gmail.com>
2019-11-21 23:38:38 -0800

    Added accelerated fall

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,12 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
     public KeyCode RightAxis = KeyCode.D;
     public KeyCode LeftAxis = KeyCode.A;
+    public KeyCode FallKey = KeyCode.S;
+    public float fallMultiplyer = 2.0f;
     public string CrouchKey = "C";
     public KeyCode JumpKey = KeyCode.Space;
     public Animator animator;
+    public Rigidbody2D rigidbody2d;
 
     public float runSpeed = 40f;
 
@@ -23,20 +26,43 @@ public class PlayerMovement : MonoBehaviour
     {
 
         //horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
-       
+
 
         if (Input.GetKey(RightAxis))
         {
+            //Right movement
             horizontalMove = 1 * runSpeed;
             animator.SetTrigger("walking");
+            //Check for accelerated and right fall
+            if (Input.GetKey(FallKey))
+            {
+                rigidbody2d.gravityScale = fallMultiplyer;
+            }
 
-        } else if (Input.GetKey(LeftAxis))
+        }
+        else if (Input.GetKey(LeftAxis))
         {
+            //Left movement
             horizontalMove = -1 * runSpeed;
             animator.SetTrigger("walking");
-        } else
+            //Check for accelerated and left fall
+            if (Input.GetKey(FallKey))
+            {
+                rigidbody2d.gravityScale = fallMultiplyer;
+            }
+        }
+        //Straight down accelerated fall
+        else if (Input.GetKey(FallKey))
+        {
+            rigidbody2d.gravityScale = fallMultiplyer;
+            animator.SetTrigger("idle");
+            horizontalMove = 0;
+        }
+        //No Keys are pressed
+        else
         {
             horizontalMove = 0;
+            rigidbody2d.gravityScale = 1.0f;
             animator.SetTrigger("idle");
         }
 
@@ -55,7 +81,7 @@ public class PlayerMovement : MonoBehaviour
             crouch = false;
         }
         */
-        
+
     }
 
     void FixedUpdate()
