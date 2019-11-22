41174ab3f4e9f9303396c61fe11257e2b2f8db0b
Andrew Motz <40045649+andrewmotz@users.noreply.github.com>
2019-11-21 17:03:37 -0800

    Added LevelTransistion Script

---
+++ Assets/Scripts/LevelTransistion.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelTransistion : MonoBehaviour
+{
+    public int sceneIndex;
+    void OnTriggerEnter2D(Collider2D other){
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
