4e2fa192cad698c6a883911bb3c28a384c761d6c
Andrew Motz <andrewmozza555@gmail.com>
2019-11-23 18:56:20 -0800

    Variable name change in bubblescript

--- Assets/Scripts/BubbleScript.cs
+++ Assets/Scripts/BubbleScript.cs
@@ -6,14 +6,14 @@ public class BubbleScript : MonoBehaviour
 {
 
     public Animator animator;
-    public GameObject GOendDoor;
-    private EndDoorLogic endDoor;
+    public GameObject EndDoor;
+    private EndDoorLogic endDoorScript;
     public static int popCount = 0;
 
     void OnTriggerEnter2D(Collider2D other){
-        endDoor = GOendDoor.GetComponent<EndDoorLogic>();
+        endDoorScript = EndDoor.GetComponent<EndDoorLogic>();
         Debug.Log("hit");
         animator.SetTrigger("hit");
-        endDoor.increaseCount();
+        endDoorScript.increaseCount();
     }
 }
--- Assets/Scripts/DoorDisable.cs
+++ Assets/Scripts/DoorDisable.cs
@@ -6,9 +6,11 @@ public class DoorDisable : MonoBehaviour
 {
     public GameObject door;
     public Animator animator;
+    public AudioSource ButtonSound;
 
     void OnTriggerEnter2D(Collider2D other){
         Debug.Log("Triggered");
+        ButtonSound.Play();
         if(other.gameObject.CompareTag("Player 1")){
             door.gameObject.SetActive(false);
             Debug.Log("Door opened");
